bc2b97a658fdf4e1b072c97df80a2d2c95b4d19a
Jaskaran Singh <jaskarans9650@gmail.com>
2024-04-30 21:44:40 +0530

    nope

--- Backend/app.js
+++ Backend/app.js
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.use(Express.static(path.join(__dirname, "/Frontend/dist")));
+app.use(Express.static(path.join(__dirname, "../Frontend/dist")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
